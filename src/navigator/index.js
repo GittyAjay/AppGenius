b5d5cf100e9436aea8f59da4a3d32058ab443784
AJAY KUMAR PANDEY <43106735+GittyAjay@users.noreply.github.com>
2024-04-23 15:51:15 +0530

    removed components

--- src/navigator/index.js
+++ src/navigator/index.js
@@ -1,9 +1,3 @@
-// App.js
-
-import ProfilePage from '../components/ProfilePage';
-import SettingsPage from '../components/SettingsPage';
-import ChatRoomPage from '../components/ChatRoomPage';
-import HomePage from '../components/HomePage';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -13,26 +7,7 @@ function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-            
-                    <Stack.Screen
-                        name="HomePage"
-                        component={HomePage}
-                    />
-            
-                    <Stack.Screen
-                        name="ChatRoomPage"
-                        component={ChatRoomPage}
-                    />
-            
-                    <Stack.Screen
-                        name="SettingsPage"
-                        component={SettingsPage}
-                    />
-            
-                    <Stack.Screen
-                        name="ProfilePage"
-                        component={ProfilePage}
-                    />
+
             </Stack.Navigator>
         </NavigationContainer>
     );
