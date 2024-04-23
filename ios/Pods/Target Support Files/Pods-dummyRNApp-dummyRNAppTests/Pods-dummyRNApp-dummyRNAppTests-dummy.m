0ad1f71562738f517be1e4000af247d6786d7f60
AJAY KUMAR PANDEY <43106735+GittyAjay@users.noreply.github.com>
2024-04-23 13:58:28 +0530

    pod install

--- ios/Pods/Target Support Files/Pods-dummyRNApp-dummyRNAppTests/Pods-dummyRNApp-dummyRNAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_dummyRNApp_dummyRNAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_dummyRNApp_dummyRNAppTests
-@end
--- ios/Pods/Target Support Files/Pods-dummyRNApp/Pods-dummyRNApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_dummyRNApp : NSObject
-@end
-@implementation PodsDummy_Pods_dummyRNApp
-@end
