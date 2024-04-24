15ead1967a30025cc9839f68c33838ccb4810d51
GittyAjay <rightmajay@gmail.com>
2024-04-25 00:25:44 +0530

    worker page design

--- src/components/ChatRoomPage.js
+++ src/components/ChatRoomPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-
+egegewgwgwegweg
 const ChatRoomPage = ({ navigation }) => {
 efwefweffwefwefwe
 
