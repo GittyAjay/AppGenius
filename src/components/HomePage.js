3cd143ad1379b9809614c8ee8710da5e5d6203c8
GittyAjay <rightmajay@gmail.com>
2024-04-25 00:05:34 +0530

    home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,19 +4,7 @@ import { View, Text, Button } from 'react-native';
 const HomePage = ({ navigation }) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to the Chat App!</Text>
-      <Button
-        title="Go to Chat Room"
-        onPress={() => navigation.navigate('ChatRoomPage')}
-      />
-      <Button
-        title="Go to Profile"
-        onPress={() => navigation.navigate('ProfilePage')}
-      />
-      <Button
-        title="Go to Settings"
-        onPress={() => navigation.navigate('SettingsPage')}
-      />
+     <Text>hello</Text>
     </View>
   );
 };
